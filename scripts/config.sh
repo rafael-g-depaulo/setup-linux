e505b5c2daf320dfddf2cc96b4cecb6fcf38d9f3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-03-01 18:19:10 -0300

    divide config, custom commands, vars and terminal

---
+++ scripts/config.sh
@@ -0,0 +1,38 @@
+# configure stuff
+
+# git and gitflow (now using hubflow instead)
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+git config --global core.editor "$GIT_EDITOR"
+# Alias for existing commands
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.br branch
+# Push only the branch you are now working on
+git config --global push.default simple
+# Make --rebase the default behavior during git pull
+git config --global pull.rebase true
+# Make --prune the default behavior during git fetch
+git config --global fetch.prune true
+# Set the width of indentation for tab characters
+git config --global core.pager 'less -x4'
+# set default branch as main
+git config --global init.defaultBranch main
+
+# idk if i want this yet
+# # Do not fast-forward when merging
+# git config --global --add merge.ff false
+# git config --global --add pull.ff only
+
+# add github to known hosts (this saves a confirmation later)
+mkdir ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# ssh keys
+isWindows && cp -r /mnt/c/Users/${USER}/.ssh ~/.
+sudo chmod 700 -R ~/.ssh
+SSH_KEY_ALG=ed25519
+ssh-keygen -q -t $SSH_KEY_ALG -C "$EMAIL" -f "$HOME/.ssh/id_$SSH_KEY_ALG" -P "" <<< n &> /dev/null
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_$SSH_KEY_ALG
+
--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-# TODO: download hyper
+function addComment() {
+  TAG="$2"
+  sed -i "s|\(\s*\)\(.*\)$TAG|\1// \2$TAG|g" $1
+}
+function removeComment() {
+  TAG="$2"
+  sed -i "s|\(\s*\)// \(.*\)$TAG| \1\2$TAG|g" $1
+}
 
-# set up hyper as default terminal
-sudo update-alternatives --set x-terminal-emulator $(which hyper)
-sudo update-alternatives --config x-terminal-emulator
+WSL_TAG="// WSL ONLY"
+LINUX_TAG="// LINUX ONLY"
+
+# get .hyper.js
+cd
+getgist rafael-g-depaulo .hyper.js <<<y &> /dev/null
+
+# if in windows, just update the hyper.js
+if isWindows; then
+  removeComment .hyper.js $WSL_TAG # uncomment all WSL specific config 
+  addComment .hyper.js $LINUX_TAG # comment out all native linux specific config
+
+  # TODO: send .hyper.js to windows folder
+else
+  sudo apt-get install -y hyper
+  removeComment .hyper.js $LINUX_TAG # uncomment all native linux specific config 
+  addComment .hyper.js $WSL_TAG # comment out all WSL specific config
+
+  # set up hyper as default terminal
+  sudo update-alternatives --set x-terminal-emulator $(which hyper)
+  sudo update-alternatives --config x-terminal-emulator
+fi
 
