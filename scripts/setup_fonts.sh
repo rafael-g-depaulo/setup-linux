61dd10c1c056902908dd4017b9c6b5f671ad2725
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-03-01 22:52:20 -0300

    refactor setupfonts

--- scripts/setup_fonts.sh
+++ scripts/setup_fonts.sh
@@ -1,7 +1,9 @@
 # download and install nerd font for fira code (with ligatures)
 
+FONT_NAME="FiraCode"
+
 RELEASE_ID=$(gh api /repos/ryanoasis/nerd-fonts/releases/latest | jq '.id')
-ASSET_ID=$(gh api /repos/ryanoasis/nerd-fonts/releases/$RELEASE_ID/assets | jq 'map(select(.name == "FiraCode.zip")) | .[0].id')
+ASSET_ID=$(gh api /repos/ryanoasis/nerd-fonts/releases/$RELEASE_ID/assets | jq "map(select(.name == \"$FONT_NAME.zip\")) | .[0].id")
 DOWNLOAD_URL=$(gh api /repos/ryanoasis/nerd-fonts/releases/assets/$ASSET_ID | jq '.browser_download_url')
 
 # my installFont custom command. check custom_commands.sh for more info 
--- visual_config.sh
+++
@@ -1,4 +0,0 @@
-# download and install nerd fonts for fira code
-
-installFont "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip"
-
