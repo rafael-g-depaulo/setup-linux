35762a955f47e734285f1548e0c2451fb73648b4
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-03-23 10:15:58 -0300

    test echo

--- scripts/custom_commands.sh
+++ scripts/custom_commands.sh
@@ -2,6 +2,12 @@
 
 # this file installs my custom commands
 
-git clone git@github.com:rafael-g-depaulo/bash-commands.git ~/commands <<<yes &> /dev/null
+git clone https://github.com/rafael-g-depaulo/bash-commands.git ~/commands
 export PATH="$PATH:$HOME/commands" # temporarily add commands to path
+
+if [ "$VAR_IS_RAGAN" == "true" ]; then 
+  echo "you are ragan"
+else
+  echo "you are not ragan, you nerd"
+fi
 
--- scripts/setup_vars.sh
+++ scripts/setup_vars.sh
@@ -7,4 +7,5 @@ export VAR_USER_PWD="${PWD:-password_here}"
 export VAR_EMAIL="${EMAIL:-rafael.g.depaulo@gmail.com}"
 export VAR_NAME="${NAME:-Rafael G. de Paulo}"
 export VAR_GIT_EDITOR="${GIT_EDITOR:-code --wait}"
+export VAR_IS_RAGAN="${IS_RAGAN:-false}"
 
