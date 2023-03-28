95e59234fdd2baa05e18002fd522b67bd01ebcce
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-03-28 14:06:48 -0300

    Add run_script function to main

--- main.sh
+++ main.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
+run_script() {
+  local SCRIPT_NAME="$1"
+  local SCRIPTS_FOLDER="$(pwd)/scripts"
+
+  source "$SCRIPTS_FOLDER/$SCRIPT_NAME"
+}
+
 # parse input and get vars
-source ./scripts/setup_vars.sh
+run_script "setup_vars.sh"
 
 # setup custom commands
-source ./scripts/custom_commands.sh
+run_script "custom_commands.sh"
 
 # cli utils
-source ./scripts/cli_utils.sh
+run_script "cli_utils.sh"
 
 # setup general config stuff
-source ./scripts/config.sh
+run_script "config.sh"
 
 # install tools
-source ./scripts/tools.sh
+run_script "tools.sh"
 
 # setup terminal emulator
 if [ $VAR_HAS_GUI = "true" ]; then
-  source ./scripts/gui_stuff.sh
-  source ./scripts/setup_fonts.sh
+  run_script "gui_stuff.sh"
+  run_script "setup_fonts.sh"
 
   if [ $VAR_INSTALL_HYPER = "true" ]; then
-    source ./scripts/terminal.sh
+    run_script "terminal.sh"
   fi
 fi
 
 # install zsh and oh-my-zsh
 if [ $VAR_INSTALL_ZSH = "true" ]; then
-  source ./scripts/zsh.sh
+  run_script "zsh.sh"
 fi
 
