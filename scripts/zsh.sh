a9fa79f8bf2d2ae636eb6fcdb4b496ad3dcc7645
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-04-05 12:31:23 -0300

    Add zsh syntax highlight

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -7,8 +7,11 @@ CHSH=yes        # update default shell
 KEEP_ZSHRC=yes  # don't override our .zshrc
 RUNZSH=yes      # run zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
+
 # oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # make zsh default shell
 chsh -s $(which zsh)
 zsh
